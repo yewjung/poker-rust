52cbafd6b01a1b85b1e75aa57efb6987ebc97022
yewjung <yewjung@gmail.com>
2025-01-25 20:14:44 +0800

    implemented side pots

--- src/service/game.rs
+++ src/service/game.rs
@@ -613,12 +613,15 @@ mod tests {
         assert_eq!(room.stage, Stage::River);
         assert_eq!(room.player_in_turn, Some(charlie.id));
         let room = service.take_action(room.id, charlie.id, Action::AllIn)?;
-        print!("{:?}", room.pots);
         assert_eq!(room.stage, Stage::River);
         let room = service.take_action(room.id, dennis.id, Action::Call)?;
 
-        // game resets to preFlop
-        assert_eq!(room.stage, Stage::PreFlop);
+        // game resets to preFlop or NotEnoughPlayers
+        if room.players.len() == 1 {
+            assert_eq!(room.stage, Stage::NotEnoughPlayers);
+        } else {
+            assert_eq!(room.stage, Stage::PreFlop);
+        }
 
         Ok(())
     }
