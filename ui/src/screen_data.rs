cddb4bfcad38ea5fbff2ada439756636dd459c05
yewjung <yewjung@gmail.com>
2025-03-06 00:19:56 +0800

    cleanup code

--- ui/src/screen_data.rs
+++ ui/src/screen_data.rs
@@ -263,7 +263,7 @@ impl StatefulWidget for InGameScreenWidget {
         Widget::render(
             Table::new(rows, Constraint::from_percentages([70, 30])).block(
                 Block::bordered()
-                    .title("Game")
+                    .title("Rooms")
                     .title_bottom(Line::from("Press Esc to quit").centered()),
             ),
             area,
